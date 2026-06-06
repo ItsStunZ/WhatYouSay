392b2472929a8b10b9572f075d9aec4906fc77ae
StunZ <115426921+ItsStunZ@users.noreply.github.com>
2026-06-06 20:34:43 +0100

    Added files

---
+++ main.js
@@ -0,0 +1,16 @@
+import { prompts } from './prompts.js';
+
+const promptElement = document.querySelector('#prompt');
+const getRandomPromptBtnElement = document.querySelector('#get-prompt-btn');
+
+
+function getRandomPrompt() {
+    return prompts[Math.floor(Math.random() * prompts.length)];
+}
+
+getRandomPromptBtnElement.addEventListener('click', (e) => {
+    promptElement.textContent = getRandomPrompt();
+});
+
+// Set prompt when page loads
+promptElement.textContent = getRandomPrompt();
